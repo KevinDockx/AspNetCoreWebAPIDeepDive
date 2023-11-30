70447050c678fffa80095d8af1f226c14c3c9592
Kevin Dockx <kevin.dockx@gmail.com>
2023-11-30 11:13:52 +0100

    Add starter files and finished solution

---
+++ Finished sample/CourseLibrary.API/Migrations/AddDateOfDeathToAuthor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseLibrary.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDateOfDeathToAuthor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "DateOfDeath",
+                table: "Authors",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("102b566b-ba1f-404c-b2df-e2cde39ade09"),
+                column: "DateOfDeath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("2902b665-1190-4c70-9915-b9c2d7680450"),
+                column: "DateOfDeath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("2aadd2df-7caf-45ab-9355-7f6332985a87"),
+                column: "DateOfDeath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("2ee49fe3-edf2-4f91-8409-3eb25ce6ca51"),
+                column: "DateOfDeath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("5b3621c0-7b12-4e80-9c8b-3398cba7ee05"),
+                column: "DateOfDeath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
+                column: "DateOfDeath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("da2fd609-d754-4feb-8acd-c4f9ff13ba96"),
+                column: "DateOfDeath",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfDeath",
+                table: "Authors");
+        }
+    }
+}
